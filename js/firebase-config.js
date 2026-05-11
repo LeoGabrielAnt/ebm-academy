d768d899dcaa6f6adb74f9aea34311c8c94738c5
Leonardo Antonio <balonciopunk@live.com>
2026-05-11 15:38:18 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,52 +1,9 @@
-{
-  "project_info": {
-    "project_number": "867364791338",
-    "project_id": "ebm-academy",
-    "storage_bucket": "ebm-academy.firebasestorage.app"
-  },
-  "client": [
-    {
-      "client_info": {
-        "mobilesdk_app_id": "1:867364791338:android:06d05bf4986a5b92c54f53",
-        "android_client_info": {
-          "package_name": "app.ebm_academy"
-        }
-      },
-      "oauth_client": [],
-      "api_key": [
-        {
-          "current_key": "AIzaSyDmgWqrU8nQNeYUp5e8oyRlo7me-dSD4R0"
-        }
-      ],
-      "services": {
-        "appinvite_service": {
-          "other_platform_oauth_client": []
-        }
-      }
-    }
-  ],
-  "configuration_version": "1"
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAQp7N4E1crXMjhHi8ECRpC3IluPthAgL4",
-    authDomain: "ebm-academy.firebaseapp.com",
-    projectId: "ebm-academy",
-    storageBucket: "ebm-academy.firebasestorage.app",
-    messagingSenderId: "867364791338",
-    appId: "1:867364791338:web:90ce68fb801c8540c54f53",
-    measurementId: "G-YEN6Q9YNEP"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+window.EBM_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAQp7N4E1crXMjhHi8ECRpC3IluPthAgL4",
+  authDomain: "ebm-academy.firebaseapp.com",
+  projectId: "ebm-academy",
+  storageBucket: "ebm-academy.firebasestorage.app",
+  messagingSenderId: "867364791338",
+  appId: "1:867364791338:web:90ce68fb801c8540c54f53",
+  measurementId: "G-YEN6Q9YNEP"
+};
